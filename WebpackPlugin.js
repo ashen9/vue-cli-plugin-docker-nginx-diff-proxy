f285b9a26ab86027665d11af624a27f30b83de02
ashen9 <247668045@qq.com>
2019-10-30 09:50:14 +0800

    1.将dockerNginxProxy配置切分为        (1)proxy_prefix(代理路径地址)        (2)env_prefix(被代理的api地址) 2.nginx各个环境的模板动态生成proxy地址 3.nginx各个环境生成的proxy地址分两种情况         (1)第一次构建项目使用插入方式         (2)非第一次构建项目使用正则替换方式(以location ${proxy_prefix} 为正则表达式)

--- WebpackPlugin.js
+++ WebpackPlugin.js
@@ -2,6 +2,26 @@ const FS = require('fs');
 const ID = 'vue-cli:nginx-proxy-webpack-Plugin'
 
 //读取文件，并且替换文件中指定的字符串
+let taskConfFile = function(filePath,targetStr,text){
+  FS.readFile(filePath,function(err,data){
+    if(err){
+      return err;
+    }
+    let str = data.toString();
+    if (str.includes(text)) {
+      var regExp = new RegExp(`${text}.*\}`, 'g');
+
+      str = str.replace(regExp, targetStr);
+      console.log(str);
+      FS.writeFile(filePath, str, function (err) {
+        if (err) return err;
+      });
+    } else {
+      resertFile(filePath, targetStr);
+    }
+  });
+}
+//读取文件，并且替换文件中指定的字符串
 let replaceFile = function(filePath,sourceRegx,targetStr){
   FS.readFile(filePath,function(err,data){
     if(err){
@@ -14,11 +34,18 @@ let replaceFile = function(filePath,sourceRegx,targetStr){
     });
   });
 }
+//向文件倒数第二行插入
+let resertFile = function (filePath, targetStr) {
+  const data = FS.readFileSync(filePath, 'utf8').split('\n');
+  data.splice(data.length - 2, 0, targetStr)
+  FS.writeFileSync(filePath, data.join('\n'), 'utf8')
+}
 
 
 module.exports = class WebpckPlugin {
   constructor (options = {}) {
-    this.options = options;
+    this.env_prefix = options.env_prefix;
+    this.proxy_prefix = options.proxy_prefix;
   }
 
   apply (compiler) {
@@ -33,7 +60,6 @@ module.exports = class WebpckPlugin {
           files.forEach((item) => {
 
             let path = nginxPath + '/' + item;
-            console.log(path);
             //判断文件的状态，用于区分文件名/文件夹
             FS.stat(path, (err, status) => {
               if (err) {
@@ -43,10 +69,14 @@ module.exports = class WebpckPlugin {
               let isDir = status.isDirectory();//是文件夹
               if (isFile) {
                 if (item.match(new RegExp(/\.conf$/))) {
-                  Object.keys(this.options).map((key) => {
+                  Object.keys(this.env_prefix).map((key) => {
                     if (item.includes(key)) {
-                      console.log(this.options[key]);
-                      replaceFile(path, /http\:\/\/localhost\:8080/g, this.options[key]); // ^ 以什么开头
+                      // replaceFile(path, /http\:\/\/localhost\:8080/g, this.env_prefix[key]); // ^ 以什么开头
+                      // replaceFile(path, /location \/proxy/g, `location ${this.proxy_prefix[key]}`); // ^ 以什么开头
+                      const text = `location ${this.proxy_prefix}`
+                      const insertText = `      ${text} { proxy_pass  ${this.env_prefix[key]} }\n`;
+                      taskConfFile(path, insertText, text);
+                      // resertFile(path, insertText);
                     }
                   });
                   // replaceFile(path,/console\.log\(\"0function0\"\)/g,"zyk");
--- generator.js
+++ generator.js
@@ -2,19 +2,23 @@ module.exports = (api, options, rootOptions) => {
   const APPNAME = rootOptions.projectName;
   let envEntity = ['dev', 'qa', 'pro'];
   let scripts = {};
-  let dockerNginxProxy = {};
+  let env_prefix = {};
+  const proxy_prefix = '/proxy';
   envEntity.map((e) => {
     const key = `docker-build-${e}`;
     scripts[key] = `docker build --build-arg ENV=${e} . -t ${APPNAME}-${e}`;
     const key2 = `docker-run-${e}`;
     scripts[key2] = `npm run ${key} && docker run -d -p 3000:80 ${APPNAME}-${e}`;
-    dockerNginxProxy[e] = 'http://localhost:8080';
+	  env_prefix[e] = 'http://localhost:8080';
   });
   api.extendPackage({
     scripts: scripts,
     vue: {
       pluginOptions: api.hasPlugin('vue-cli-plugin-docker-nginx-diff-proxy') ? {} : {
-        dockerNginxProxy: dockerNginxProxy
+        dockerNginxProxy: {
+	        proxy_prefix,
+	        env_prefix
+        }
       }
     }
   });
